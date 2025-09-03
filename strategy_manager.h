1a688f3cc7866dcf9423602cef8c5e0b110f620d
rhondaoj4 <rhondadev4@gmail.com>
2025-09-03 14:53:31 -0400

    handle and manage trading strategies in strategies_ map

---
+++ strategy_manager.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "strategy.h"
+#include "simdjson.h"
+#include <memory>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class StrategyManager{
+    
+public:
+
+    // add new trading strategies to manager's collection
+    void add_strategy(const string &name, shared_ptr<IStrategy> strategy);
+    // iterates through every strategy stored in strategies_ and makes sure
+    //it's processed simultaneously 
+    void process_data(const simdjson::dom::element &doc);
+
+private:
+
+    // different strategies -> implementation
+    map<string, shared_ptr<IStrategy>> strategies_;
+
+};

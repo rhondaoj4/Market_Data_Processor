ef27eef75d33a881e61324c2fb4d4c33bae85c14
rhondaoj4 <rhondadev4@gmail.com>
2025-09-03 14:44:04 -0400

    Add modular strategy integration

--- strategy.h
+++ strategy.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <string>
+#include "simdjson.h"
+
+using namespace std;
+
+//abstract class
+class IStrategy{
+
+    //ensures correct destructor from a derived class is called
+    virtual ~IStrategy() = default;
+
+    //force other classes to implement on_market_data by itself
+    virtual void on_market_data(const simdjson::dom::element &doc) = 0;
+
+    //force other classes to implement on_tick by itself
+    virtual void on_tick(const std::string &symbol, double price, double quantity) = 0;
+};
